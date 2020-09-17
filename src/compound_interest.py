03804e459077dc27bfbf1ca8e7f53e123f4ea678
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-09-17 14:39:02 +0100

    initial tests for principal, years and interest added and passing ok

--- src/compound_interest.py
+++ src/compound_interest.py
@@ -1,2 +1,7 @@
 class CompoundInterest:
-    pass
+
+    def __init__(self, amount, interest, years):
+        self.amount = amount 
+        self.years = years
+        self.interest = interest
+        self.n = 12 #nr of times interest is compounded per year
--- tests/compound_interest_test.py
+++ tests/compound_interest_test.py
@@ -4,9 +4,26 @@ from src.compound_interest import CompoundInterest
 
 class CompoundInterestTest(unittest.TestCase):
 
+    # def setUp(self):
+    #     self.compound_interest = CompoundInterest()
+   
+
     # Tests
+    
+    def test_get_principal_amount(self):
+        self.compound_interest = CompoundInterest(100.00, 0.10, 20)
+        self.assertEqual(100, self.compound_interest.amount)
+
+    def test_get_interest_rate(self):
+        self.compound_interest = CompoundInterest(100.00, 0.10, 20)
+        self.assertEqual(0.10, self.compound_interest.interest)
+
+    def test_get_number_of_years(self):
+        self.compound_interest = CompoundInterest(100.00, 0.10, 20)
+        self.assertEqual(20, self.compound_interest.years)
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
+    # def test_compound_interest_function_ten_percent_twenty_years(self,)
 
     # Should return 181.94 given 100 principal, 6 percent, 10 years
 
